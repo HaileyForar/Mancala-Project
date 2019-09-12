3ff3c898edfcad0bcf9cdf07e3090bd03b3ed51a
Hailey.forar <Hailey.forar@MU-C1-20.mullen.pvt.k12.co.us>
2019-09-12 11:07:27 -0600

    First commit - need to start coding game

---
+++ 2D Array Project - Mancala Game/src/MancalaGame.java
@@ -0,0 +1,11 @@
+
+public class MancalaGame
+	{
+
+		public static void main(String[] args)
+			{
+				System.out.println("In this game you will be playing an African marble game called Mancala. Have fun!");
+
+			}
+
+	}
